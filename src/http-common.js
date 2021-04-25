bb9151ebc5e3af316f4a982e6233570c4017372b
heliopiovezan <heliopiovezan@yahoo.com.br>
2021-04-25 19:19:11 -0300

    Alteração do direcionamento da api de local para remoto.

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3000/',
-  // baseURL: 'https://igti-gradesapi.herokuapp.com/',
+  // baseURL: 'http://localhost:3000/',
+  baseURL: 'https://igti-gradesapi.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
